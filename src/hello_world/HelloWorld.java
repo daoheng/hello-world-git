13fa2a7353de7deea1e4c36fb3aee4c18c74d4c0
daoheng guo <guodh9999@163.com>
2015-03-17 13:56:32 -0400

    i did a new line

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		
+		System.out.println("made change for git");
+		
 		Card testCard=new Card(10,"queen");
 		Card testCard2=new Card(10);
 		
